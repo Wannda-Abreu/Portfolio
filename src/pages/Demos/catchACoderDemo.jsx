1f7573afd2456c9106b7c65af105656347cd2181
Wanda <wandamarketingspecialist@gmail.com>
2024-01-29 02:33:12 +0100

    final changes

--- src/pages/Demos/catchACoderDemo.jsx
+++ src/pages/Demos/catchACoderDemo.jsx
@@ -6,6 +6,7 @@ import "./demo.css";
 import { Link } from "react-router-dom";
 
 function catchACoderDemo() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const videoRef = useRef(null);
 
   const handlePlayVideo = () => {
