f60c992479da5a451b326bbaa736358d018fcaa6
Wanda <wandamarketingspecialist@gmail.com>
2024-01-30 18:34:04 +0100

    feat(main): adding final features

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -10,6 +10,7 @@ import AboutMe from "../pages/AboutMe/aboutMe";
 import MyCv from "../pages/Cv/cv";
 import ContactForm from "../pages/Contact/ContactForm";
 
+
 const router = createBrowserRouter([
     {
       path: "/",
