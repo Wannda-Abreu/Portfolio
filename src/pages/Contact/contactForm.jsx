4b8c09951a4748b6c294335619e421664516411b
Wanda <wandamarketingspecialist@gmail.com>
2024-01-31 21:14:49 +0100

    feat(main): adding final chnges ton contact form

--- src/pages/Contact/contactForm.jsx
+++ src/pages/Contact/contactForm.jsx
@@ -1,54 +1,64 @@
-import {
-  Form,
-  FormGroup,
-  FormControl,
-  FormLabel,
-  Container,
-} from "react-bootstrap";
-import "./contactForm.css";
+import { useForm, ValidationError } from '@formspree/react';
+import { Form, FormGroup, FormControl, FormLabel, Container} from 'react-bootstrap';
+import './contactForm.css';
+
 
 function ContactForm() {
+  const [state, handleSubmit] = useForm("mrgnevql");
+
+  if (state.succeeded) {
+    return (
+      <div className="contact-message">
+        <h4>¡Mensaje Enviado! 🥳</h4> 
+        <p className='mt-3'>¡Su mensaje ha sido enviado correctamente! Me pondré en contacto contigo lo más pronto posible. ¡Saludos! 💻</p>
+      </div>
+    );
+  }
+  
+
   return (
     <Container className="form-wrapper">
-      <Form action="https://formspree.io/f/mrgnevql" method="POST">
+      <Form onSubmit={handleSubmit}>
         <FormGroup className="form-name-group">
-          <FormLabel htmlFor="formName">Nombre</FormLabel>
+          <FormLabel htmlFor="name">Nombre</FormLabel>
           <FormControl
             type="text"
-            id="formName"
+            id="name"
             name="name"
             placeholder="Nombre y apellido"
             required
             autoComplete="name"
           />
+          <ValidationError prefix="Nombre" field="name" errors={state.errors} />
         </FormGroup>
 
         <FormGroup className="form-email-group mt-2">
-          <FormLabel htmlFor="formEmail">Correo Electrónico</FormLabel>
+          <FormLabel htmlFor="email">Correo Electrónico</FormLabel>
           <FormControl
             type="email"
-            id="formEmail"
-            name="_replyto"
+            id="email"
+            name="email"
             placeholder="ejemplo@dominio.com"
             required
             autoComplete="email"
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </FormGroup>
 
         <FormGroup className="form-message-group mt-2">
-          <FormLabel htmlFor="formMessage">Mensaje</FormLabel>
+          <FormLabel htmlFor="message">Mensaje</FormLabel>
           <FormControl
             as="textarea"
-            id="formMessage"
+            id="message"
             name="message"
             placeholder="Información adicional"
             rows={3}
             required
-            autoComplete="off"
           />
+          <ValidationError prefix="Mensaje" field="message" errors={state.errors} />
         </FormGroup>
 
-        <button className="contact-button mt-4" type="submit">
+        <button className="contact-button mt-4" type="submit" disabled={state.submitting}>
           Enviar
         </button>
       </Form>
