b581bde307d5158e7567144656827465d3aab631
Wanda <wandamarketingspecialist@gmail.com>
2024-02-01 15:03:23 +0100

    feat(main): adding final changes to contact form6

--- src/pages/Cv/cv.jsx
+++ src/pages/Cv/cv.jsx
@@ -7,14 +7,14 @@ function MyCv() {
     <div className="cv-container mt-1">
       <img
         className="cv-image mt-5 img-fluid"
-        src="https://res.cloudinary.com/dqj4pvyva/image/upload/q_auto:best/v1706662914/Original_size_Blue_Clean_Professional_CV_Resume_vtl3wo.avif"
+        src="https://res.cloudinary.com/dqj4pvyva/image/upload/v1706795715/wanda-cv-image_pxibs1.avif"
         alt="Curriculum Vitae"
       ></img>
       <div className="cv-button mt-1 mb-2">
         <button>
           <a className="cv-button"
       
-        href="https://wandacv.my.canva.site/-cv-wanda-abreu"
+        href="https://wandacv.my.canva.site/"
             target="_blank"
             rel="noopener noreferrer"
           >
