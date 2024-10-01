6f1c42dc13b51e5e7f9820660d8249127082146d
Wanda <wandamarketingspecialist@gmail.com>
2024-10-01 15:17:28 +0200

    new about me document cv download pdf button

--- src/pages/Cv/cv.jsx
+++ src/pages/Cv/cv.jsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExpand } from "@fortawesome/free-solid-svg-icons";
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css'; // Asegúrate de importar los estilos de zoom
 import "./cv.css";
@@ -7,7 +5,6 @@ import "./cv.css";
 function MyCv() {
   return (
     <div className="cv-container mt-3">
-      
       <Zoom>
         <img
           className="cv-image mt-2 img-fluid"
@@ -15,8 +12,19 @@ function MyCv() {
           alt="Curriculum Vitae"
         />
       </Zoom>
+
+      <div className="download-container mt-3">
+        <a 
+          href="https://res.cloudinary.com/dsyfal3wa/image/upload/q_auto:best/v1727787680/Original_size_Original_size_Original_size_Original_size_Original_size_Original_size_Blue_Clean_Professional_CV_Resume_ju2lmj.pdf" 
+          className="btn btn-primary"
+          download 
+        >
+          Descargar CV en PDF
+        </a>
+      </div>
     </div>
   );
 }
 
 export default MyCv;
+
