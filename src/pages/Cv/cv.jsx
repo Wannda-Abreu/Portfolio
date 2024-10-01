d9cdbfe23c28613ca048272d92ff6aeb3cf3b568
Wanda <wandamarketingspecialist@gmail.com>
2024-10-01 15:07:40 +0200

    new about me document cv

--- src/pages/Cv/cv.jsx
+++ src/pages/Cv/cv.jsx
@@ -1,15 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExpand } from "@fortawesome/free-solid-svg-icons";
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css'; // Asegúrate de importar los estilos de zoom
 import "./cv.css";
 
 function MyCv() {
   return (
     <div className="cv-container mt-3">
-      <img
-        className="cv-image mt-2 img-fluid"
-        src="https://res.cloudinary.com/dsyfal3wa/image/upload/q_auto:best/v1727787680/Original_size_Original_size_Original_size_Original_size_Original_size_Original_size_Blue_Clean_Professional_CV_Resume_ju2lmj.avif"
-        alt="Curriculum Vitae"
-      ></img>
+      
+      <Zoom>
+        <img
+          className="cv-image mt-2 img-fluid"
+          src="https://res.cloudinary.com/dsyfal3wa/image/upload/q_auto:best/v1727787680/Original_size_Original_size_Original_size_Original_size_Original_size_Original_size_Blue_Clean_Professional_CV_Resume_ju2lmj.avif"
+          alt="Curriculum Vitae"
+        />
+      </Zoom>
+      
       <div className="cv-button mt-2 mb-2">
         <button>
           <a
