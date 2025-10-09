eaf10d27ed5167f9928d09f5a11a64af34577bde
Wanda Abreu Díaz <137044379+Wannda-Abreu@users.noreply.github.com>
2025-10-09 17:20:16 +0200

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,6 +12,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+
+import cvFile from "../WandaAbreu-CV-WebDeveloper.pdf";
 
 function CustomNavbar() {
   const [scrolled, setScrolled] = useState(false);
@@ -54,10 +56,19 @@ function CustomNavbar() {
               <FontAwesomeIcon icon={faMugHot} className="nav-icon" />
               Sobre mí
             </Nav.Link>
-            <Nav.Link as={Link} to="/mycv" className="nav-link" onClick={closeNavbar}>
+
+            {/* ✅ Link directo al PDF importado */}
+            <Nav.Link
+              href={cvFile}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+              onClick={closeNavbar}
+            >
               <FontAwesomeIcon icon={faSeedling} className="nav-icon" />
               CV
             </Nav.Link>
+
             <Nav.Link as={Link} to="/projects" className="nav-link" onClick={closeNavbar}>
               <FontAwesomeIcon icon={faCodeBranch} className="nav-icon" />
               Proyectos
@@ -66,7 +77,12 @@ function CustomNavbar() {
               <FontAwesomeIcon icon={faAddressBook} className="nav-icon" />
               Contáctame
             </Nav.Link>
-            <Nav.Link as={Link} to="/certifications" className="nav-link" onClick={closeNavbar}>
+            <Nav.Link
+              as={Link}
+              to="/certifications"
+              className="nav-link"
+              onClick={closeNavbar}
+            >
               <FontAwesomeIcon icon={faCertificate} className="nav-icon" />
               Certificados educativos
             </Nav.Link>
@@ -78,5 +94,3 @@ function CustomNavbar() {
 }
 
 export default CustomNavbar;
-
-
