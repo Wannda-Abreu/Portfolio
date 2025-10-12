1c36293d3308866bf9e7ff61a4a725d993b7dff3
Wanda <wandamarketingspecialist@gmail.com>
2025-10-13 01:33:57 +0200

    Remove CV download action and rely on preview image

---
+++ src/constants/cvLinks.js
@@ -0,0 +1,7 @@
+const CLOUDINARY_BASE = "https://res.cloudinary.com/dqj4pvyva";
+const CV_PUBLIC_ID = "wanda-abreu-Cv-_web_mlrdhc";
+const CV_IMAGE_EXTENSION = "avif";
+const VERSION = "v1759972967";
+
+export const CV_IMAGE_URL = `${CLOUDINARY_BASE}/image/upload/${VERSION}/${CV_PUBLIC_ID}.${CV_IMAGE_EXTENSION}`;
+export const CV_PREVIEW_URL = CV_IMAGE_URL;
--- src/pages/Cv/cv.jsx
+++ src/pages/Cv/cv.jsx
@@ -1,33 +1,23 @@
+import { CV_IMAGE_URL, CV_PREVIEW_URL } from '../../constants/cvLinks';
 import './cv.css';
 
-const CV_IMG =
-  'https://res.cloudinary.com/dqj4pvyva/image/upload/v1759972967/wanda-abreu-Cv-_web_mlrdhc.avif';
-
 export default function MyCv() {
-  const downloadUrl = `${CV_IMG}?fl_attachment=Wanda_Abreu_CV`;
-
   return (
     <section className="cv-page">
       <div className="cv-toolbar">
         <a
-          href={CV_IMG}
+          href={CV_PREVIEW_URL}
           target="_blank"
           rel="noopener noreferrer"
           className="cv-btn"
         >
           Ver a pantalla completa
         </a>
-        <a
-          href={downloadUrl}
-          className="cv-btn cv-btn-primary"
-        >
-          Descargar CV
-        </a>
       </div>
 
       <div className="cv-stage">
         <img
-          src={CV_IMG}
+          src={CV_IMAGE_URL}
           alt="CV de Wanda Abreu"
           className="cv-img"
           loading="eager"
